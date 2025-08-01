d29075ca122b12674aeb2087c6432ee9b7f22284
chaiwat <advicebanphot@gmail.com>
2025-08-01 12:38:17 +0700

    NextRequest

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
 // middleware.ts
 import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-export function middleware() {
+export function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 
